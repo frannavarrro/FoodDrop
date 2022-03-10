4cb082791acc1205df191e353d7338867a153350
Francisco <deutschundenglisch@gmail.com>
2022-03-10 08:17:32 -0300

    Main Loop implementado

--- FoodDrop/MainWindow.xaml.cs
+++ FoodDrop/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace FoodDrop
 {
@@ -24,17 +26,70 @@ namespace FoodDrop
         {
             InitializeComponent();
 
+            corriendo = false;
+
             canvas.Focus();
+
+            mainLoopHandler = new DispatcherTimer();
+
+            mainLoopHandler.Tick += new EventHandler(mainLoop);
         }
 
         private void canvasKeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Space)
             {
-                titulo.Visibility = Visibility.Collapsed;
+                if (!corriendo)
+                {
+                    titulo.Visibility = Visibility.Collapsed;
 
-                menuLabel.Visibility = Visibility.Collapsed;
+                    menuLabel.Visibility = Visibility.Collapsed;
+
+                    jugador.Visibility = Visibility.Visible;
+
+                    corriendo = true;
+
+                    mainLoopHandler.Start();
+
+                }
             }
         }
+
+        private void mainLoop(object sender, EventArgs e)
+        {
+            double inicio = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+
+            procesarInput();
+
+            actualizar();
+
+            renderizar();
+
+            double espera = inicio + 16 - DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+
+            Thread.Sleep((int)espera);
+ 
+        }
+
+        private void procesarInput()
+        {
+
+        }
+
+        private void actualizar()
+        {
+
+        }
+
+        private void renderizar()
+        {
+   
+            
+        }
+
+        private bool corriendo;
+
+        private DispatcherTimer mainLoopHandler;
+
     }
 }
